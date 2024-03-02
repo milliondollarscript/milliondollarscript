d5227ea94d00929379469a8ef7e37941f7679c1e
Ryan <575452-rwr@users.noreply.gitlab.com>
2024-03-02 18:08:39 -0500

    Add Cron stub class to prevent errors from refactors.

---
+++ src/Classes/Cron.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Million Dollar Script Two
+ *
+ * @author      Ryan Rhode
+ * @copyright   (C) 2024, Ryan Rhode
+ * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *    Million Dollar Script
+ *    Pixels to Profit: Ignite Your Revolution
+ *    https://milliondollarscript.com/
+ *
+ */
+
+namespace MillionDollarScript\Classes;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Stub for Cron class.
+ *
+ * Necessary for upgrades to be able to run without errors from previous version due to class refactoring.
+ *
+ */
+class Cron {
+	public static function add_minute( $schedules ): mixed {
+		return Cron::add_minute( $schedules );
+	}
+
+	public static function schedule_cron(): void {
+		Cron::schedule_cron();
+	}
+
+	public static function clear_cron(): void {
+		Cron::clear_cron();
+	}
+
+	public static function minute(): void {
+		Cron::minute();
+	}
+
+	public static function clean_temp_files(): void {
+		Cron::clean_temp_files();
+	}
+}
