348d212bbf8a4aaaf0015cb497a45784034bd298
Ryan <575452-rwr@users.noreply.gitlab.com>
2024-03-02 18:05:52 -0500

    Add DatabaseStatic stub class to prevent errors from refactors.

---
+++ src/Classes/DatabaseStatic.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Million Dollar Script Two
+ *
+ * @author      Ryan Rhode
+ * @copyright   (C) 2024, Ryan Rhode
+ * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *    Million Dollar Script
+ *    Pixels to Profit: Ignite Your Revolution
+ *    https://milliondollarscript.com/
+ *
+ */
+
+namespace MillionDollarScript\Classes;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Stub for DatabaseStatic class.
+ *
+ * Necessary for upgrades to be able to run without errors from previous version due to class refactoring.
+ *
+ */
+class DatabaseStatic {
+	public static function convert_version( string $version ): string {
+		return \MillionDollarScript\Classes\Data\DatabaseStatic::convert_version( $version );
+	}
+
+	public static function table_exists( $table_name ): bool {
+		return \MillionDollarScript\Classes\Data\DatabaseStatic::table_exists( $table_name );
+	}
+}
