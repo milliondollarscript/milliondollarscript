4f7cbf6915fa46105acefb417a39820f0eb3698e
Ryan <575452-rwr@users.noreply.gitlab.com>
2025-05-03 03:25:04 -0400

    Fix database schema issues when upgrading from 2.3.5
    - Add missing 'config_key' column to wp_mds_config table (or rename from 'key')
    - Add missing 'enabled' column to wp_mds_banners table
    - Add missing 'order_in_progress' column to wp_mds_orders table
    - Update MDS_DB_VERSION to match plugin version

--- milliondollarscript-two.php
+++ milliondollarscript-two.php
@@ -62,7 +62,7 @@ defined( 'MDS_CORE_URL' ) or define( 'MDS_CORE_URL', MDS_BASE_URL . 'src/Core/' 
 defined( 'MDS_TEXT_DOMAIN' ) or define( 'MDS_TEXT_DOMAIN', 'milliondollarscript' );
 defined( 'MDS_PREFIX' ) or define( 'MDS_PREFIX', 'milliondollarscript_' );
 defined( 'MDS_DB_PREFIX' ) or define( 'MDS_DB_PREFIX', $wpdb->prefix . 'mds_' );
-defined( 'MDS_DB_VERSION' ) or define( 'MDS_DB_VERSION', '2.5.11.0' );
+defined( 'MDS_DB_VERSION' ) or define( 'MDS_DB_VERSION', '2.5.11.40' );
 defined( 'MDS_VERSION' ) or define( 'MDS_VERSION', '2.5.11.40' );
 
 // Detect PHP version
---
+++ src/Upgrades/_2_5_11_40.php
@@ -0,0 +1,111 @@
+<?php
+
+/*
+ * Million Dollar Script Two
+ *
+ * @author      Ryan Rhode
+ * @copyright   (C) 2025, Ryan Rhode
+ * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *    Million Dollar Script
+ *    Pixels to Profit: Ignite Your Revolution
+ *    https://milliondollarscript.com/
+ *
+ */
+
+namespace MillionDollarScript\Upgrades;
+
+use MillionDollarScript\Classes\Data\Database;
+use MillionDollarScript\Classes\Data\DatabaseStatic;
+
+defined( 'ABSPATH' ) or exit;
+
+/** @noinspection PhpUnused */
+
+class _2_5_11_40 {
+
+    /**
+     * Upgrade the database schema to version 2.5.11.40
+     * 
+     * This upgrade adds missing columns that were causing errors when upgrading from 2.3.5:
+     * - Fixes the 'key' to 'config_key' column in wp_mds_config table (prioritized)
+     * - Adds 'enabled' column to wp_mds_banners table
+     * - Adds 'order_in_progress' column to wp_mds_orders table
+     *
+     * @param string $version Current database version
+     * 
+     * @return void
+     */
+    public function upgrade( $version ): void {
+        if ( version_compare( $version, '2.5.11.40', '<' ) ) {
+            global $wpdb;
+            
+            // 1. PRIORITY: Fix config_key column issue first
+            // This is critical because other parts of the code rely on this column
+            $this->ensure_config_key_column();
+            
+            // 2. Add 'enabled' column to banners table if it doesn't exist
+            $table_name = MDS_DB_PREFIX . 'banners';
+            if (DatabaseStatic::table_exists($table_name)) {
+                $column_exists = $wpdb->get_var( "SHOW COLUMNS FROM `{$table_name}` LIKE 'enabled'" );
+                
+                if ( ! $column_exists ) {
+                    $wpdb->query( "ALTER TABLE `{$table_name}` ADD COLUMN `enabled` ENUM('Y','N') NOT NULL DEFAULT 'Y' AFTER `banner_id`" );
+                    $wpdb->flush();
+                }
+            }
+            
+            // 3. Add 'order_in_progress' column to orders table if it doesn't exist
+            $table_name = MDS_DB_PREFIX . 'orders';
+            if (DatabaseStatic::table_exists($table_name)) {
+                $column_exists = $wpdb->get_var( "SHOW COLUMNS FROM `{$table_name}` LIKE 'order_in_progress'" );
+                
+                if ( ! $column_exists ) {
+                    $wpdb->query( "ALTER TABLE `{$table_name}` ADD COLUMN `order_in_progress` ENUM('Y','N') NOT NULL DEFAULT 'N' AFTER `user_id`" );
+                    $wpdb->flush();
+                }
+            }
+        }
+    }
+    
+    /**
+     * More thorough method to ensure config_key column exists instead of 'key'
+     * 
+     * @return void
+     */
+    private function ensure_config_key_column(): void {
+        global $wpdb;
+        $table_name = MDS_DB_PREFIX . 'config';
+        
+        // Check if table exists first
+        if (DatabaseStatic::table_exists($table_name)) {
+            // Direct approach to check for 'key' column and rename it
+            $key_column_exists = $wpdb->get_var( "SHOW COLUMNS FROM `{$table_name}` LIKE 'key'" );
+            $config_key_column_exists = $wpdb->get_var( "SHOW COLUMNS FROM `{$table_name}` LIKE 'config_key'" );
+            
+            if ($key_column_exists && !$config_key_column_exists) {
+                // Rename the column from 'key' to 'config_key'
+                $wpdb->query( "ALTER TABLE `{$table_name}` CHANGE `key` `config_key` VARCHAR(100) NOT NULL DEFAULT ''" );
+                $wpdb->flush();
+            } else if (!$key_column_exists && !$config_key_column_exists) {
+                // If neither column exists, create config_key
+                $wpdb->query( "ALTER TABLE `{$table_name}` ADD COLUMN `config_key` VARCHAR(100) NOT NULL DEFAULT '' FIRST" );
+                $wpdb->flush();
+            }
+        }
+    }
+}
